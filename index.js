343db11349f8f7e5cd282e3413b6026f591dbca0
Feruzk <38233905+Feruzk@users.noreply.github.com>
2019-03-05 19:58:34 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+const Joi = require('joi')
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+]
+
+
+app.get('/', (req, res) => {
+   //Test output
+   res.send('Hello World!!!');
+});
+
+
+app.get('/api/courses', (req, res) => {
+    
+    //Display all the courses
+    res.send(courses); 
+});
+
+
+app.post('/api/courses', (req, res) => {
+       
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+
+})
+
+
+app.put('/api/courses/:id', (req, res) => {
+
+    //Look up the coure
+    //If ott existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course is not found -Put method')
+
+    //Validate
+    //If invalid, return 400 - Bad request
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    //Update cours
+    //Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+
+})
+
+
+app.get('/api/courses/:id', (req, res) => {
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course is not found -Get method')
+    res.send(course);
+
+});
+
+
+app.delete('/api/courses/:id', (req, res) => {
+
+    //Look up the course
+    //If ott existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course is not found -Delete method')
+    
+    //Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);    
+
+    //Return the same course
+    res.send(course);
+});
+
+
+function validateCourse(course){
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+    
+}
+
+
+const port = process.env.POST || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
